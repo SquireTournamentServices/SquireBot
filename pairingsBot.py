2a041220b9e6652ade363ad6d67cf2624b277336
TylerBloom <tyler.bloom.ctr@us.af.mil>
2020-12-05 21:24:07 -0500

    Initial bot and auth token.

---
+++ pairingsBot.py
@@ -0,0 +1,34 @@
+# bot.py
+import os
+import random
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    print(ctx.guild.name +" " + ctx.channel)
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(TOKEN)
