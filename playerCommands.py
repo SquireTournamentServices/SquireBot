db5dab6a8e11fec8e1d1e05c3666d4508f275ca9
Danny Piper <djpiper28@gmail.com>
2021-06-21 20:03:49 +0100

    Changed add-deck error handling.

--- playerCommands.py
+++ playerCommands.py
@@ -223,11 +223,22 @@ async def submitDecklist( ctx, tourn = None, ident = None ):
         await ctx.send( f'{mention}, not enough information provided: Please provide your deckname and decklist to add a deck.' )
         return
     
-    message = await tournObj.addDeck( ctx.author.id, ident, decklist )
+    message = ""
+    try:
+        message = await tournObj.addDeck( ctx.author.id, ident, decklist )
+        if isMoxFieldLink(decklist):
+            await ctx.send( f'{mention}, please be aware that moxfield treats your commander as if it were in your sideboard.' )
+        await ctx.send( f'{mention}, {message}' )
+        if not private:
+            await ctx.author.send( f'For future reference, you can submit your decklist via private message so that you do not have to publicly post your decklist.' )
+    except SyntaxError as ex:
+        traceback.print_exception(type(ex), ex, ex.__traceback__)
+        await ctx.send( f'{mention}, there was an error while processing your deck list, it is possible that it is in the wrong format. Make sure you follow the instructions for submitting a deck. To find them, use "!squirebot-help add-deck".' )
+        return
     await ctx.send( f'{mention}, {message}' )
     if not private:
         await ctx.author.send( f'For future reference, you can submit your decklist via private message so that you do not have to publicly post your decklist.' )
-        
+    await tournObj.updateInfoMessage()
         
         
 commandSnippets["remove-deck"] = "- remove-deck : Removes a deck you registered (can be DM-ed)" 
