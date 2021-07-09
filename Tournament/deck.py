7df836c3af69017ca3cfc486f715d9fe123b1e9a
Danny Piper <djpiper28@gmail.com>
2021-07-09 16:02:01 +0100

    Correct indentation that broke moxfield sideboards
    fix my fix

--- Tournament/deck.py
+++ Tournament/deck.py
@@ -162,12 +162,13 @@ class deck:
             # Add card to decklist
             card = main_board[card_name]
             self.decklist += f'{card["quantity"]} {card_name}\n'
-            side_board = deck_data["sideboard"]
-            for card_name in side_board:
-                # Add card to decklist
-                card = side_board[card_name]
-                self.decklist += f'SB: {card["quantity"]} {card_name}\n'
-
+            
+        side_board = deck_data["sideboard"]            
+        for card_name in side_board:
+            # Add card to decklist
+            card = side_board[card_name]
+            self.decklist += f'SB: {card["quantity"]} {card_name}\n'
+                
         if not self.validateDecklist( self.decklist ):
             raise DeckRetrievalError( f'Error while retrieving a deck from {url}' )
 
