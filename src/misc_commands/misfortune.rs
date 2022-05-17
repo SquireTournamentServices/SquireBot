27941072e1927b10e06331c5a465c2ba9eb25406
TylerBloom <tylerbloom2222@gmail.com>
2022-05-17 15:51:20 -0400

    Rewrote misfortune command. Compiles!

--- src/misc_commands/misfortune.rs
+++ src/misc_commands/misfortune.rs
@@ -1,9 +1,14 @@
 use std::collections::HashSet;
 
-use crate::model::{
-    containers::{MisfortuneMapContainer, MisfortunePlayerMapContainer, TournamentMapContainer},
-    lookup_error::LookupError,
-    misfortune::*,
+use crate::{
+    model::{
+        containers::{
+            GuildAndTournamentIDMapContainer, MisfortuneMapContainer, MisfortuneUserMapContainer,
+            TournamentMapContainer, TournamentNameAndIDMapContainer,
+        },
+        misfortune::*,
+    },
+    utils::tourn_resolver::tourn_id_resolver,
 };
 
 use serenity::{
@@ -23,6 +28,7 @@ use squire_core::{
 #[max_args(1)]
 #[description("Helps you resolve Wheel of Misfortune.")]
 async fn misfortune(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    /* Old version
     let data = ctx.data.read().await;
     let mis_players = data.get::<MisfortunePlayerMapContainer>().unwrap();
     if let Some(r_id) = mis_players.get(&msg.author.id) {
@@ -56,6 +62,8 @@ async fn misfortune(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
             .await?;
     }
     Ok(())
+    */
+    todo!()
 }
 
 #[command("create")]
@@ -64,61 +72,73 @@ async fn misfortune(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 #[max_args(1)]
 #[description("Start resolving Wheel of Misfortune.")]
 async fn create(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    // TODO: Make resolver for user to tournament mapping
-    // let data = ctx.data.read().await;
-    // let all_tourns = data.get::<TournamentContainer>().unwrap();
-    // let gld_tourns = data.get::<GuildTournamentsContainer>().unwrap();
-    // let local_tourns = gld_tourns.get(&msg.guild_id.unwrap()).unwrap();
+    /* Old version */
     let data = ctx.data.read().await;
     let all_tourns = data.get::<TournamentMapContainer>().unwrap();
-    let gld_tourns = data.get::<GuildTournamentsMapContainer>().unwrap();
-    let local_tourns = gld_tourns.get(&msg.guild_id.unwrap()).unwrap();
-    let name_and_tourn = user_to_tourn(all_tourns, &local_tourns, &ctx.http, &msg, &args).await?;
-    // Do some tournament queries
-    let p_id = name_and_tourn.value().get_player_id(msg.author.id).unwrap();
-    let tourn_id = name_and_tourn.value().get_id();
-    let tourn = all_tourns.get_tourn(TournIdentifier::Id(tourn_id)).unwrap();
-    let r_id = match tourn.get_player_round(PlayerIdentifier::Id(p_id)) {
-        Err(e) => {
-            msg.reply(&ctx.http, "You aren't in an active match.")
-                .await?;
-            Err(e)?
+    let name_and_id = data.get::<TournamentNameAndIDMapContainer>().unwrap();
+    let gld_tourns = data.get::<GuildAndTournamentIDMapContainer>().unwrap();
+    let given_name = args.rest();
+    let id_iter = gld_tourns
+        .get_left_iter(&msg.guild_id.unwrap())
+        .unwrap()
+        .cloned();
+    let tourn_id = match tourn_id_resolver(&ctx, &msg, &given_name, &name_and_id, id_iter).await {
+        Some(id) => id,
+        None => {
+            return Ok(());
         }
-        Ok(r) => r,
     };
-    let round = tourn.value().get_round(RoundIdentifier::Id(r_id)).unwrap();
-    let players: HashSet<UserId> = round
-        .get_all_players()
-        .iter()
-        .filter_map(|p| name_and_tourn.value().get_user_id(p.clone()))
-        .collect();
-    let player_misfortunes = data.get::<MisfortunePlayerContainer>().unwrap();
-    for p in &players {
-        match ctx.http.get_user(p.0).await {
-            Err(e) => {
-                // This shouldn't happen...
-                msg.reply(
-                    &ctx.http,
-                    "There was an error in finding one of your opponents.",
-                )
-                .await?;
-                Err(e)?
-            }
-            Ok(u) => {
-                u.dm(&ctx.http, |m| {
-                    m.content(
-                        "Please use the `!misfortune` command to resolve the Wheel of Misfortune.",
-                    )
-                })
-                .await?;
-            }
+    let tourn = all_tourns.get(&tourn_id).unwrap();
+    let plyr_id = match tourn.get_player_id(&msg.author.id) {
+        Some(id) => id,
+        None => {
+            let _ = msg
+                .reply(&ctx.http, "You are not registered for that tournament.")
+                .await;
+            return Ok(());
         }
-        player_misfortunes.insert(p.clone(), r_id.clone());
+    };
+    let round_id = match tourn
+        .tourn
+        .get_player_round(&PlayerIdentifier::Id(plyr_id.clone()))
+    {
+        Ok(id) => id,
+        Err(_) => {
+            let _ = msg
+                .reply(
+                    &ctx.http,
+                    "You are not in an active match of that tournament.",
+                )
+                .await;
+            return Ok(());
+        }
+    };
+    let mut user_round_map = data.get::<MisfortuneUserMapContainer>().unwrap().write().await;
+    let round = tourn
+        .tourn
+        .get_round(&RoundIdentifier::Id(round_id))
+        .unwrap();
+    user_round_map.insert_right(round_id.clone());
+    let mut mis = Misfortune::new(HashSet::new(), msg.channel_id, msg.id);
+    for plyr in round.get_all_players() {
+        let user = tourn.get_user_id(&plyr).unwrap();
+        user_round_map.insert_left(user.clone(), &round_id);
+        mis.players.insert(user);
+        let _ = msg
+            .guild(&ctx)
+            .unwrap()
+            .member(&ctx, user)
+            .await
+            .unwrap()
+            .user
+            .dm(&ctx, |m| {
+                m.content("Enter in your misfortune number with `!misfortune`")
+            })
+            .await;
     }
-    let mis = Misfortune::new(players, msg.channel_id, msg.id);
-    let all_misfortunes = data.get::<MisfortuneContainer>().unwrap();
-    all_misfortunes.insert(r_id, mis);
-    msg.reply(&ctx.http, "Respond with your number via DM!")
+    let misforunes = data.get::<MisfortuneMapContainer>().unwrap();
+    misforunes.insert(round_id, mis);
+    msg.reply(&ctx.http, "Respond to the DM to record your response.")
         .await?;
     Ok(())
 }
