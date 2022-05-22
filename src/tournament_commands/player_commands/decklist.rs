35b017df0f7dae119235fb1ceeaf25ffd01ecdd2
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-05-22 12:37:29 -0400

    started work on decklist command

--- src/tournament_commands/player_commands/decklist.rs
+++ src/tournament_commands/player_commands/decklist.rs
@@ -5,5 +5,35 @@ use serenity::prelude::*;
 #[command("decklist")]
 #[description("Prints out one of your decklists.")]
 async fn decklist(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+     /* Get references to needed data from context */
+     let data = ctx.data.read().await;
+     let name_and_id = data.get::<TournamentNameAndIDMapContainer>().unwrap();
+     let gld_tourns = data.get::<GuildAndTournamentIDMapContainer>().unwrap();
+     let player_name = msg.author.id.0.to_string();
+
+     let id_iter = gld_tourns.get_left_iter(&msg.guild_id.unwrap()).unwrap();
+     let tourn_id = match id_iter.len() {
+         0 => {
+             msg.reply(
+                 &ctx.http,
+                 "There are no tournaments being held in this server.",
+             )
+             .await;
+         },
+         1 => id_iter.next.unwrap(),
+         _ => {
+             
+         }
+     };
+
+     let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+     let tourn = all_tourns.get_mut(tourn_id).unwrap();
+
+     if(len(tourn.players[player_name].decklists) == 0) {
+         msg.reply(
+             &ctx.http,
+             "You have not registered any decks for this tournament"
+         )
+         .await;
+     }
 }
