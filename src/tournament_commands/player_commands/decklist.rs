b365e556d255726ee39f36a23cd5501a7e3ef413
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-05-24 11:55:01 -0400

    Update src/tournament_commands/player_commands/decklist.rs
    Co-authored-by: Tyler Bloom <tylerbloom2222@gmail.com>

--- src/tournament_commands/player_commands/decklist.rs
+++ src/tournament_commands/player_commands/decklist.rs
@@ -8,7 +8,8 @@ async fn decklist(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
      /* Get references to needed data from context */
      let data = ctx.data.read().await;
      let name_and_id = data.get::<TournamentNameAndIDMapContainer>().unwrap();
-     let gld_tourns = data.get::<GuildAndTournamentIDMapContainer>().unwrap();
+let gld_tourn_ids =  data.get::<GuildAndTournamentIDMapContainer>().unwrap();
+let tourns =  data.get::<TournamentMapContainer>().unwrap();
      let player_name = msg.author.id.0.to_string();
 
      let id_iter = gld_tourns.get_left_iter(&msg.guild_id.unwrap()).unwrap();
