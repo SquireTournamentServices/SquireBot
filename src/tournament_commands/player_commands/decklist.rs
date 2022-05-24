b7f0f8dede83aacd31f316085d8b4b5fdd69aa61
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-05-24 15:12:34 -0400

    continued working on decklist command
    ended up rewriting large portions of this function because it turns out i was doing the wrong thing. also fixed some other errors in my code

--- src/tournament_commands/player_commands/decklist.rs
+++ src/tournament_commands/player_commands/decklist.rs
@@ -1,49 +1,81 @@
 use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::http::error::ErrorResponse;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use serenity::utils::MessageBuilder;
+use squire_core::player_registry::PlayerIdentifier;
+
+
+use crate::model::{
+    containers::{
+        GuildAndTournamentIDMapContainer, TournamentMapContainer, TournamentNameAndIDMapContainer,
+    },
+    guild_tournament::GuildTournament,
+};
 
 #[command("decklist")]
 #[description("Prints out one of your decklists.")]
-async fn decklist(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
+async fn decklist(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
      /* Get references to needed data from context */
      let data = ctx.data.read().await;
      let name_and_id = data.get::<TournamentNameAndIDMapContainer>().unwrap();
-let gld_tourn_ids =  data.get::<GuildAndTournamentIDMapContainer>().unwrap();
-let tourns =  data.get::<TournamentMapContainer>().unwrap();
-     let player_name = msg.author.id.0.to_string();
-
-     let id_iter = gld_tourns.get_left_iter(&msg.guild_id.unwrap()).unwrap().filter(|id| tourn.get(id).unwrap().players.contains(msg.user.id));
-     let tourn_id = match id_iter.len() {
+     let gld_tourn_ids = data.get::<GuildAndTournamentIDMapContainer>().unwrap();
+     let tourns = data.get::<TournamentMapContainer>().unwrap();
+     let user_name = msg.author.id;
+     let deck_name = args.single_quoted::<String>().unwrap();
+     let tourn_name = args.rest().trim().to_string();
+     let id_iter = gld_tourn_ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().filter(|id| tourns.get(id).unwrap().players.contains_left(&msg.author.id));
+     let tourn_id = match id_iter.count() {
          0 => {
              msg.reply(
                  &ctx.http,
-                 "There are no tournaments being held in this server.",
+                 "You are not registered for any tournaments in this server.",
              )
              .await;
+             return Ok(());
          },
-         1 => id_iter.next.unwrap(),
+         1 => id_iter.next().unwrap(),
          _ => {
-             
+            if let Some(id) = 
+            id_iter.find(|id| name_and_id.get_left(id).unwrap() == &tourn_name) 
+            {
+                id
+            } 
+            else 
+            {
+                msg.reply(
+                    &ctx.http,
+                    "You are not registered for a tournament with that name."
+                )
+                .await?;
+                return Ok(())
+            }
          }
      };
-
+     
      let all_tourns = data.get::<TournamentMapContainer>().unwrap();
      let tourn = all_tourns.get_mut(tourn_id).unwrap();
 
-     if(tourn.players[player_name].decks.len == 0) {
-         msg.reply(
-             &ctx.http,
-             "You have not registered any decks for this tournament"
-         )
-         .await;
-     }
-
-     let names = tourn.players[player_name].decks.names;
-     let hashes = tourn.players[player_name].decks.hashes;
-     let response = MessageBuilder::new();
-     for name in names {
-         response
-         .push_bold_safe(name)
-         .push("\n")
-     }
+     let player_id = tourn.players.get_right(&user_name).unwrap().clone();
+     let player = tourn.tourn.get_player(&PlayerIdentifier::Id(player_id)).unwrap();
+     let response = String::new();
+     match player.get_deck(deck_name) {
+        Some(Deck) => {
+            let mainboard = Deck.get_mainboard();
+            for card in mainboard {
+                
+            }
+            msg.reply(
+                &ctx.http,
+                response
+            ).await?;
+        },
+        None => {
+            msg.reply (
+            &ctx.http,
+            "Could not find a deck with that name"
+            ).await?;
+        }
+    }
+    Ok(())
 }
