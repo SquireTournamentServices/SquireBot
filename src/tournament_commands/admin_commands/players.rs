2f631ba697f527e6badeaec7c52ba2296934a3fb
TylerBloom <tylerbloom2222@gmail.com>
2022-06-04 17:38:30 -0400

    Added admin players command

--- src/tournament_commands/admin_commands/players.rs
+++ src/tournament_commands/admin_commands/players.rs
@@ -1,11 +1,75 @@
-use serenity::framework::standard::{macros::command, Args, CommandResult};
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+use serenity::{
+    async_trait,
+    framework::standard::{macros::command, Args, CommandResult},
+    model::prelude::*,
+    prelude::*,
+};
+
+use squire_core::{
+    operations::TournOp, player_registry::PlayerIdentifier, tournament::TournamentId,
+};
+
+use crate::{
+    model::{
+        confirmation::Confirmation,
+        containers::{
+            ConfirmationsContainer, GuildAndTournamentIDMapContainer, TournamentMapContainer,
+            TournamentNameAndIDMapContainer,
+        },
+    },
+    utils::{
+        embeds::embed_fields,
+        error_to_reply::error_to_reply,
+        tourn_resolver::{admin_tourn_id_resolver, player_name_resolver, user_id_resolver},
+    },
+};
 
 #[command("players")]
 #[only_in(guild)]
 #[allowed_roles("Tournament Admin", "Judge")]
 #[description("Prints out a list of all players.")]
-async fn players(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+async fn players(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+    let name_and_id = data
+        .get::<TournamentNameAndIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let ids = data
+        .get::<GuildAndTournamentIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+    let mut id_iter = ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().cloned();
+    // Resolve the tournament id
+    let tourn_name = args.rest().trim().to_string();
+    let tourn_id = match admin_tourn_id_resolver(ctx, msg, &tourn_name, &name_and_id, id_iter).await
+    {
+        Some(id) => id,
+        None => {
+            return Ok(());
+        }
+    };
+    let tourn = all_tourns.get(&tourn_id).unwrap();
+    let name_iter = tourn
+        .tourn
+        .player_reg
+        .players
+        .iter()
+        .map(|(id, _)| player_name_resolver(id.clone(), &tourn.players, &tourn.tourn));
+    let len = name_iter.len();
+    let fields = embed_fields(name_iter)
+        .into_iter()
+        .map(|f| ("Players", f, false));
+    match msg.channel(&ctx.http).await? {
+        Channel::Guild(c) => {
+            c.send_message(&ctx.http, |m| {
+                m.embed(|e| e.title(format!("Players: ({len})")).fields(fields))
+            })
+            .await?;
+        }
+        _ => {}
+    };
+    Ok(())
 }
