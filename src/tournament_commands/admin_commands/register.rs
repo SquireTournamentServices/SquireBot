b3efebf7c0a03b9527c2a201f4bbb7a22163e834
TheDeveloper101 <32082053+TheDeveloper101@users.noreply.github.com>
2022-06-05 20:01:38 -0400

    attempted implementation of the admin registration command

--- src/tournament_commands/admin_commands/register.rs
+++ src/tournament_commands/admin_commands/register.rs
@@ -2,10 +2,63 @@ use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
+use crate::model::{
+    containers::{
+        GuildAndTournamentIDMapContainer, TournamentMapContainer, TournamentNameAndIDMapContainer,
+    },
+    guild_tournament::GuildTournament,
+};
+
+use squire_core::operations::TournOp;
+use crate::utils::error_to_reply::error_to_reply;
+use crate::utils::tourn_resolver::{admin_tourn_id_resolver, user_id_resolver};
+
 #[command("register")]
 #[only_in(guild)]
 #[allowed_roles("Tournament Admin", "Judge")]
 #[description("Registers a player on their behalf.")]
-async fn register(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+async fn register(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+    let name_and_id = data
+        .get::<TournamentNameAndIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let ids = data
+        .get::<GuildAndTournamentIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+    let mut id_iter = ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().cloned();
+    // Resolve the tournament id
+    let raw_user_id = args.single_quoted::<String>().unwrap();
+    let user_id = match user_id_resolver(ctx, msg, &raw_user_id).await {
+        Some(id) => id,
+        None => {
+            return Ok(());
+        }
+    };
+    let tourn_name = args.rest().trim().to_string();
+    let tourn_id = match admin_tourn_id_resolver(ctx, msg, &tourn_name, &name_and_id, id_iter).await
+    {
+        Some(id) => id,
+        None => {
+            return Ok(());
+        }
+    }; 
+    
+    let mut tourn = all_tourns.get_mut(&tourn_id).unwrap();
+    
+    match tourn.add_player(raw_user_id, user_id) {
+        Ok(_) => {
+            msg.reply(
+                &ctx.http,
+                format!("You have been registered for {}", tourn.tourn.name),
+            )
+            .await?;
+            Ok(())
+        }
+        Err(e) => error_to_reply(ctx, msg, e).await,
+    }
 }
