e62a74a4dfb43ce9e94ea5b54360a694f9429bc6
TylerBloom <tylerbloom2222@gmail.com>
2022-06-08 19:08:13 -0400

    Impl-ed player add deck, name, (un)ready, and remove deck commands.

--- src/tournament_commands/player_commands/add_deck.rs
+++ src/tournament_commands/player_commands/add_deck.rs
@@ -1,9 +1,104 @@
-use serenity::framework::standard::{macros::command, Args, CommandResult};
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+use serenity::{
+    framework::standard::{macros::command, Args, CommandResult},
+    model::prelude::*,
+    prelude::*,
+};
+
+use squire_core::{operations::TournOp, player_registry::PlayerIdentifier};
+
+use crate::{
+    model::containers::{
+        CardCollectionContainer, GuildAndTournamentIDMapContainer, TournamentMapContainer,
+        TournamentNameAndIDMapContainer,
+    },
+    utils::{
+        error_to_reply::error_to_reply,
+        tourn_resolver::{admin_tourn_id_resolver, user_id_resolver},
+    },
+};
 
 #[command("add-deck")]
 #[description("Submits a deck.")]
-async fn add_deck(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+async fn add_deck(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+    let name_and_id = data
+        .get::<TournamentNameAndIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let ids = data
+        .get::<GuildAndTournamentIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+    let mut id_iter = ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().cloned();
+    // Resolve the tournament id
+    let deck_name = args.single_quoted::<String>().unwrap();
+    let raw_deck = args.single_quoted::<String>().unwrap();
+    let tourn_name = args.rest().trim().to_string();
+    let mut id_iter = ids
+        .get_left_iter(&msg.guild_id.unwrap())
+        .unwrap()
+        .filter(|id| {
+            all_tourns
+                .get(id)
+                .unwrap()
+                .players
+                .contains_left(&msg.author.id)
+        });
+    let tourn_id = match id_iter.clone().count() {
+        0 => {
+            msg.reply(
+                &ctx.http,
+                "You are not registered for any tournaments in this server.",
+            )
+            .await?;
+            return Ok(());
+        }
+        1 => id_iter.next().unwrap(),
+        _ => {
+            if let Some(id) = id_iter.find(|id| name_and_id.get_left(id).unwrap() == &tourn_name) {
+                id
+            } else {
+                msg.reply(
+                    &ctx.http,
+                    "You are not registered for a tournament with that name.",
+                )
+                .await?;
+                return Ok(());
+            }
+        }
+    };
+    let mut tourn = all_tourns.get_mut(&tourn_id).unwrap();
+    let plyr_id = match tourn.players.get_right(&msg.author.id) {
+        Some(id) => PlayerIdentifier::Id(id.clone()),
+        None => {
+            msg.reply(
+                &ctx.http,
+                "You are not registered for that tournament.",
+            )
+            .await?;
+            return Ok(());
+        }
+    };
+    let card_coll = data.get::<CardCollectionContainer>().unwrap().read().await;
+    let deck = if let Some(deck) = card_coll.create_deck(raw_deck.clone()) {
+        deck
+    } else if let Some(deck) = card_coll.import_deck(raw_deck).await {
+        deck
+    } else {
+        msg.reply(&ctx.http, "Unable to create a deck from this.")
+            .await?;
+        return Ok(());
+    };
+    if let Err(err) = tourn
+        .tourn
+        .apply_op(TournOp::AddDeck(plyr_id, deck_name, deck))
+    {
+        error_to_reply(ctx, msg, err).await?;
+    } else {
+        msg.reply(&ctx.http, "Deck successfully added!").await?;
+    }
+    Ok(())
 }
--- src/tournament_commands/player_commands/ready.rs
+++ src/tournament_commands/player_commands/ready.rs
@@ -1,19 +1,170 @@
-use serenity::framework::standard::{macros::command, Args, CommandResult};
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+use serenity::{
+    framework::standard::{macros::command, Args, CommandResult},
+    model::prelude::*,
+    prelude::*,
+};
+
+use squire_core::{operations::TournOp, player_registry::PlayerIdentifier};
+
+use crate::{
+    model::containers::{
+        CardCollectionContainer, GuildAndTournamentIDMapContainer, TournamentMapContainer,
+        TournamentNameAndIDMapContainer,
+    },
+    utils::{
+        error_to_reply::error_to_reply,
+        tourn_resolver::{admin_tourn_id_resolver, user_id_resolver},
+    },
+};
 
 #[command("ready")]
 #[only_in(guild)]
 #[aliases("lfg")]
 #[description("Shows that you're ready to play your next match.")]
-async fn ready(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+async fn ready(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+    let name_and_id = data
+        .get::<TournamentNameAndIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let ids = data
+        .get::<GuildAndTournamentIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+    let mut id_iter = ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().cloned();
+    // Resolve the tournament id
+    let tourn_name = args.rest().trim().to_string();
+    let mut id_iter = ids
+        .get_left_iter(&msg.guild_id.unwrap())
+        .unwrap()
+        .filter(|id| {
+            all_tourns
+                .get(id)
+                .unwrap()
+                .players
+                .contains_left(&msg.author.id)
+        });
+    let tourn_id = match id_iter.clone().count() {
+        0 => {
+            msg.reply(
+                &ctx.http,
+                "You are not registered for any tournaments in this server.",
+            )
+            .await?;
+            return Ok(());
+        }
+        1 => id_iter.next().unwrap(),
+        _ => {
+            if let Some(id) = id_iter.find(|id| name_and_id.get_left(id).unwrap() == &tourn_name) {
+                id
+            } else {
+                msg.reply(
+                    &ctx.http,
+                    "You are not registered for a tournament with that name.",
+                )
+                .await?;
+                return Ok(());
+            }
+        }
+    };
+    let mut tourn = all_tourns.get_mut(&tourn_id).unwrap();
+    let plyr_id = match tourn.players.get_right(&msg.author.id) {
+        Some(id) => PlayerIdentifier::Id(id.clone()),
+        None => {
+            msg.reply(
+                &ctx.http,
+                "You are not registered for that tournament.",
+            )
+            .await?;
+            return Ok(());
+        }
+    };
+    if let Err(err) = tourn
+        .tourn
+        .apply_op(TournOp::ReadyPlayer(plyr_id))
+    {
+        error_to_reply(ctx, msg, err).await?;
+    } else {
+        msg.reply(&ctx.http, "Deck successfully added!").await?;
+    }
+    Ok(())
 }
 
 #[command("unready")]
 #[only_in(guild)]
 #[aliases("leave-lfg")]
 #[description("Shows that you're not ready to play your next match.")]
-async fn unready(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+async fn unready(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+    let name_and_id = data
+        .get::<TournamentNameAndIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let ids = data
+        .get::<GuildAndTournamentIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+    let mut id_iter = ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().cloned();
+    // Resolve the tournament id
+    let tourn_name = args.rest().trim().to_string();
+    let mut id_iter = ids
+        .get_left_iter(&msg.guild_id.unwrap())
+        .unwrap()
+        .filter(|id| {
+            all_tourns
+                .get(id)
+                .unwrap()
+                .players
+                .contains_left(&msg.author.id)
+        });
+    let tourn_id = match id_iter.clone().count() {
+        0 => {
+            msg.reply(
+                &ctx.http,
+                "You are not registered for any tournaments in this server.",
+            )
+            .await?;
+            return Ok(());
+        }
+        1 => id_iter.next().unwrap(),
+        _ => {
+            if let Some(id) = id_iter.find(|id| name_and_id.get_left(id).unwrap() == &tourn_name) {
+                id
+            } else {
+                msg.reply(
+                    &ctx.http,
+                    "You are not registered for a tournament with that name.",
+                )
+                .await?;
+                return Ok(());
+            }
+        }
+    };
+    let mut tourn = all_tourns.get_mut(&tourn_id).unwrap();
+    let plyr_id = match tourn.players.get_right(&msg.author.id) {
+        Some(id) => PlayerIdentifier::Id(id.clone()),
+        None => {
+            msg.reply(
+                &ctx.http,
+                "You are not registered for that tournament.",
+            )
+            .await?;
+            return Ok(());
+        }
+    };
+    if let Err(err) = tourn
+        .tourn
+        .apply_op(TournOp::UnReadyPlayer(plyr_id))
+    {
+        error_to_reply(ctx, msg, err).await?;
+    } else {
+        msg.reply(&ctx.http, "Deck successfully added!").await?;
+    }
+    Ok(())
 }
