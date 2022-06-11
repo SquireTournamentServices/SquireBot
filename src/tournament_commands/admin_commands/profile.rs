1c9dee810bd1d34e9961bb0daf14312d616b6070
TylerBloom <tylerbloom2222@gmail.com>
2022-06-11 17:51:11 -0400

    Added player profile command

--- src/tournament_commands/admin_commands/profile.rs
+++ src/tournament_commands/admin_commands/profile.rs
@@ -1,11 +1,96 @@
-use serenity::framework::standard::{macros::command, Args, CommandResult};
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+use itertools::Itertools;
+use serenity::{
+    framework::standard::{macros::command, Args, CommandResult},
+    model::prelude::*,
+    prelude::*,
+};
+
+use squire_core::{operations::TournOp, player::PlayerStatus, player_registry::PlayerIdentifier};
+
+use crate::{
+    model::containers::{
+        CardCollectionContainer, GuildAndTournamentIDMapContainer, TournamentMapContainer,
+        TournamentNameAndIDMapContainer,
+    },
+    utils::{
+        error_to_reply::error_to_reply,
+        tourn_resolver::{admin_tourn_id_resolver, user_id_resolver},
+    },
+};
 
 #[command("profile")]
 #[only_in(guild)]
 #[allowed_roles("Tournament Admin", "Judge")]
 #[description("Prints out the profile of a player.")]
-async fn profile(ctx: &Context, msg: &Message, _: Args) -> CommandResult {
-    todo!()
+async fn profile(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let data = ctx.data.read().await;
+    let name_and_id = data
+        .get::<TournamentNameAndIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let ids = data
+        .get::<GuildAndTournamentIDMapContainer>()
+        .unwrap()
+        .read()
+        .await;
+    let all_tourns = data.get::<TournamentMapContainer>().unwrap();
+    let mut id_iter = ids.get_left_iter(&msg.guild_id.unwrap()).unwrap().cloned();
+    // Resolve the tournament id
+    let raw_user_id = args.single_quoted::<String>().unwrap();
+    let user_id = match user_id_resolver(ctx, msg, &raw_user_id).await {
+        Some(id) => id,
+        None => {
+            return Ok(());
+        }
+    };
+    let tourn_name = args.rest().trim().to_string();
+    let tourn_id = match admin_tourn_id_resolver(ctx, msg, &tourn_name, &name_and_id, id_iter).await
+    {
+        Some(id) => id,
+        None => {
+            return Ok(());
+        }
+    };
+    let tourn = all_tourns.get(&tourn_id).unwrap();
+    let plyr_id = match tourn.players.get_right(&user_id) {
+        Some(id) => PlayerIdentifier::Id(id.clone()),
+        None => {
+            msg.reply(
+                &ctx.http,
+                "That player is not registered for the tournament.",
+            )
+            .await?;
+            return Ok(());
+        }
+    };
+    let plyr = tourn.tourn.get_player(&plyr_id).unwrap();
+    let rounds = tourn.tourn.get_player_rounds(&plyr_id).unwrap();
+    match msg.channel(&ctx.http).await.unwrap() {
+        Channel::Guild(c) => {
+            c.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    if let Some(tag) = plyr.game_name {
+                        e.field("Game Tag:", tag, false);
+                    }
+                    e.title(format!("<@{}> Profile", user_id))
+                        .field(
+                            "Status:",
+                            match plyr.status {
+                                PlayerStatus::Registered => "Registered",
+                                PlayerStatus::Dropped => "Dropped",
+                            },
+                            false,
+                        )
+                        .field("Decks:", plyr.decks.keys().join("\n"), false)
+                        .field("Matches:", rounds.iter().map(|r| format!("Match #{}", r.match_number)).join("\n"), false)
+                })
+            })
+            .await?;
+        }
+        _ => {
+            unreachable!("Can't send message from non guild channel.");
+        }
+    }
+    Ok(())
 }
