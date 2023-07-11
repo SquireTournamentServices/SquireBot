21de0db750a7ce51af6c27e59590d2c556830b4c
Dora Akbulut <akbulutdora@gmail.com>
2023-07-11 15:59:26 +0100

    fix populate embed

--- src/model/tourn_settings_tree.rs
+++ src/model/tourn_settings_tree.rs
@@ -1,60 +1,97 @@
 use serenity::builder::CreateEmbed;
 
-use squire_lib::settings::TournamentSettingsTree;
+use squire_lib::settings::{
+    FluidPairingSettingsTree, GeneralSettingsTree, PairingSettingsTree, PairingStyleSettingsTree,
+    ScoringSettingsTree, ScoringStyleSettingsTree, TournamentSettingsTree,
+};
 
 pub fn populate_embed<'a>(
     tree: &TournamentSettingsTree,
     embed: &'a mut CreateEmbed,
 ) -> &'a mut CreateEmbed {
-    let data = format!("Format: {}\nFirst Table Number: {}\nTable Numbers?: {}\nMin. Deck Count: {}\nMax. Deck Count: {}\nRequire Checkin?: {}\nRequire Decks?: {}\n",
-    tree.format,
-    tree.starting_table_number,
-    tree.use_table_numbers,
-    tree.min_deck_count,
-    tree.max_deck_count,
-    tree.require_check_in,
-    tree.require_deck_reg,);
-    embed.field("Base Tournament Settings:", data, true);
-
-    let data = format!("Match Size: {}\nRepair Tolerance: {}\nPairing Algorithm: {}\n\n**Swiss Settings**:\nDo Checkins?: {}\n\n**Fluid Settings**:",
-    tree.pairing_settings.match_size,
-    tree.pairing_settings.repair_tolerance,
-    tree.pairing_settings.algorithm,
-    tree.pairing_settings.swiss.do_check_ins);
-    embed.field("Pairing Settings:", data, true);
-
-    let data = format!(
-        "**Standard Scoring**\n\
-    Match Win: {}\n\
-    Match Draw: {}\n\
-    Match Loss: {}\n\
-    Game Win: {}\n\
-    Game Draw: {}\n\
-    Game Loss: {}\n\
-    Byes: {}\n\
-    Include Byes: {}\n\
-    Include MP: {}\n\
-    Include GP: {}\n\
-    Include MWP: {}\n\
-    Include GWP: {}\n\
-    Include OMWP: {}\n\
-    Include OGWP: {}",
-        tree.scoring_settings.standard.match_win_points,
-        tree.scoring_settings.standard.match_draw_points,
-        tree.scoring_settings.standard.match_loss_points,
-        tree.scoring_settings.standard.game_win_points,
-        tree.scoring_settings.standard.game_draw_points,
-        tree.scoring_settings.standard.game_loss_points,
-        tree.scoring_settings.standard.bye_points,
-        tree.scoring_settings.standard.include_byes,
-        tree.scoring_settings.standard.include_match_points,
-        tree.scoring_settings.standard.include_game_points,
-        tree.scoring_settings.standard.include_mwp,
-        tree.scoring_settings.standard.include_gwp,
-        tree.scoring_settings.standard.include_opp_mwp,
-        tree.scoring_settings.standard.include_opp_gwp,
-    );
-    embed.field("Scoring Settings:", data, true);
+    populate_general_embed(&tree.general, embed);
+    populate_pairing_embed(&tree.pairing, embed);
+    populate_scoring_embed(&tree.scoring, embed);
 
     embed
+}
+
+fn populate_general_embed<'a>(tree: &GeneralSettingsTree, embed: &'a mut CreateEmbed) {
+    let data = format!(
+        "Format: {}\n
+        First Table Number: {}\n
+        Table Numbers?: {}\n
+        Min. Deck Count: {}\n
+        Max. Deck Count: {}\n
+        Require Checkin?: {}\n
+        Require Decks?: {}\n",
+        tree.format,
+        tree.starting_table_number,
+        tree.use_table_number,
+        tree.min_deck_count,
+        tree.max_deck_count,
+        tree.require_check_in,
+        tree.require_deck_reg,
+    );
+    embed.field("Base Tournament Settings:", data, true);
+}
+
+fn populate_pairing_embed<'a>(tree: &PairingSettingsTree, embed: &'a mut CreateEmbed) {
+    let common_data = format!(
+        "Match Size: {}\n
+        Repair Tolerance: {}\n
+        Pairing Algorithm: {}\n\n",
+        tree.common.match_size, tree.common.repair_tolerance, tree.common.algorithm,
+    );
+    let style_data = match tree.style {
+        PairingStyleSettingsTree::Swiss(tree) => {
+            format!(
+                "**Swiss Settings**:\n
+                Do Checkins?: {}",
+                tree.do_checkins,
+            )
+        }
+        PairingStyleSettingsTree::Fluid(FluidPairingSettingsTree {}) => {
+            format!("**Fluid Settings**:")
+        }
+    };
+    let data = format!("{}{}", common_data, style_data);
+    embed.field("Pairing Settings:", data, true);
+}
+
+fn populate_scoring_embed(tree: &ScoringSettingsTree, embed: &mut CreateEmbed) {
+    let style_data = match tree.style {
+        ScoringStyleSettingsTree::Standard(tree) => format!(
+            "**Standard Scoring**\n\
+        Match Win: {}\n\
+        Match Draw: {}\n\
+        Match Loss: {}\n\
+        Game Win: {}\n\
+        Game Draw: {}\n\
+        Game Loss: {}\n\
+        Byes: {}\n\
+        Include Byes: {}\n\
+        Include MP: {}\n\
+        Include GP: {}\n\
+        Include MWP: {}\n\
+        Include GWP: {}\n\
+        Include OMWP: {}\n\
+        Include OGWP: {}",
+            tree.match_win_points,
+            tree.match_draw_points,
+            tree.match_loss_points,
+            tree.game_win_points,
+            tree.game_draw_points,
+            tree.game_loss_points,
+            tree.bye_points,
+            tree.include_byes,
+            tree.include_match_points,
+            tree.include_game_points,
+            tree.include_mwp,
+            tree.include_gwp,
+            tree.include_opp_mwp,
+            tree.include_opp_gwp,
+        ),
+    };
+    embed.field("Scoring Settings:", style_data, true);
 }
